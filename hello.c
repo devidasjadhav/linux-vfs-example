09c5fe1ebd9b24fcbc3a3a7f1a1890c6ac3e5125
Devidas Jadhav <freekepange@gmail.com>
2019-01-22 23:39:10 +0530

    SysFS get and set

--- hello.c
+++ hello.c
@@ -2,7 +2,30 @@
 #include <linux/kernel.h>
 #include <linux/init.h>  
 
+MODULE_LICENSE("GPLV2");     
+MODULE_AUTHOR("Devidas"); 
+MODULE_DESCRIPTION("SysFS get set"); 
+MODULE_VERSION("0.1"); 
 
+
+int sysfs_test_set(const char *val, const struct kernel_param *kp){
+	printk(KERN_INFO "SET:%s\n",val);
+	return 0;
+}
+int sysfs_test_get(char *buffer, const struct kernel_param *kp){
+	strcpy(buffer,"Hello");
+	return 5;
+}
+
+
+const struct kernel_param_ops sysfs_test_ops = 
+{
+    .set = &sysfs_test_set, // Use our setter ...
+    .get = &sysfs_test_get
+};
+//unsigned short param = 0xff;
+char param[20]= {0};
+module_param_cb(test,&sysfs_test_ops,&param,S_IRUGO | S_IWUSR);
 
 
 static int __init hello_init(void)
